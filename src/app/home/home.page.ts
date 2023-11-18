5dadfcdd309736f5509925b317ec992cec69f81c
PedroCasacchi1 <pedro.casacchi@edu.unifil.br>
2023-11-18 14:42:06 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,96 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  resultado: string = "0";
+  memoria: string = "";
+  verifica_zero: boolean = true;
+  operador_inserido: boolean = false;
+  is_segundo_elemento: boolean = false;
+  primeiro_elemento: string = "";
+  segundo_elemento: string = "";
+  operador: string = "";
+  is_novo_calculo: boolean = false;
 
+
+  constructor() { }
+
+  digitos(valor: string) {
+    if (this.is_novo_calculo) {
+      this.resetar();
+      if(this.is_segundo_elemento){
+        this.segundo_elemento += valor;
+        this.resultado += valor;
+      } else {
+        if (this.verifica_zero) {
+          this.resultado = valor;
+          this.verifica_zero = false;
+        } else {
+          this.resultado += valor;
+        }
+      }
+    } else {
+      if(this.is_segundo_elemento){
+        this.segundo_elemento += valor;
+        this.resultado += valor;
+      } else {
+        if (this.verifica_zero) {
+          this.resultado = valor;
+          this.verifica_zero = false;
+        } else {
+          this.resultado += valor;
+        }
+      }
+    }
+
+  }
+
+  operadores(operador: string) {
+    if (!this.operador_inserido && this.verifica_zero == false) {
+      this.primeiro_elemento = this.resultado;
+      this.resultado += operador;
+      this.operador_inserido = true;
+      this.operador = operador;
+      this.is_segundo_elemento = true;
+    }
+  }
+
+  calcular() {
+
+    if (this.operador == "+" && this.segundo_elemento != "") {
+      this.resultado = (parseInt(this.primeiro_elemento) + parseInt(this.segundo_elemento)).toString();
+      this.memoria = this.primeiro_elemento + this.operador + this.segundo_elemento + "=" + this.resultado;
+      this.is_novo_calculo = true;
+    } else if (this.operador == "-" && this.segundo_elemento != "") {
+      this.resultado = (parseInt(this.primeiro_elemento) - parseInt(this.segundo_elemento)).toString();
+      this.memoria = this.primeiro_elemento + this.operador + this.segundo_elemento + "=" + this.resultado;
+      this.is_novo_calculo = true;
+    } else if (this.operador == "*" && this.segundo_elemento != "") {
+      this.resultado = (parseInt(this.primeiro_elemento) * parseInt(this.segundo_elemento)).toString();
+      this.memoria = this.primeiro_elemento + this.operador + this.segundo_elemento + "=" + this.resultado;
+      this.is_novo_calculo = true;
+    } else if (this.operador == "/" && this.segundo_elemento != "") {
+      this.resultado = (parseInt(this.primeiro_elemento) / parseInt(this.segundo_elemento)).toString();
+      this.memoria = this.primeiro_elemento + this.operador + this.segundo_elemento + "=" + this.resultado;
+      this.is_novo_calculo = true;
+    } else if (this.operador == "√"){
+        this.resultado = (Math. sqrt(parseInt(this.primeiro_elemento))) .toString();
+    }else {
+      if (this.operador == "") {
+        alert("Nenhum operador foi selecionado.")
+      } else {
+        alert("O segundo elemento não foi definido.")
+      }
+    }
+  }
+
+  resetar() {
+    this.resultado = "0";
+    this.verifica_zero = true;
+    this.operador_inserido = false;
+    this.is_segundo_elemento = false;
+    this.primeiro_elemento = "";
+    this.segundo_elemento = "";
+    this.operador = "";
+    this.is_novo_calculo = false;
+  }
 }
